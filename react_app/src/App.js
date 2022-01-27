c1b8a6472a95f3f91259253939f257eb77b76e4d
Kento Ichikawa <kent@tahoiya.org>
2022-01-27 16:39:29 +0900

    onClick draw Rect

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -3,39 +3,57 @@ import Rect from './Rect.js'
 import './App.css';
 
 class App extends Component {
+   data = [];
+
    msgStyle = {
-      fontSize:"24pt",
+      fontSize:"20pt",
       color:"#900",
       margin:"20px 0px",
       padding: "5px",
-      borderBottom: "2px solid #900"
    }
-   btnStyle = {
-      fontSize:"20pt",
-      padding:"0px 10px"
+   area = {
+      width:"500px",
+      height:"500px",
+      border:"1px solid blue"
    }
    constructor(props){
       super(props);
       this.state = {
-         msg:'Hello from kent',
+         list:this.data
       };
-      let time = setInterval(()=>{
-         this.setState((state)=>({
-            msg: state.msg + "!"
-         }));
-      }, 3000);
+      this.doAction = this.doAction.bind(this);
+   }
+   doAction(e){
+      let x = e.pageX;
+      let y = e.pageY;
+      this.data.push({x:x,y:y});
+      this.setState({
+         list:this.data
+      });
    }
 
-   render() {
+   draw(d){
+      let s = {
+         position:"absolute",
+         left:(d.x - 25)+"px",
+         top:(d.y -25)+"px",
+         width:"50px",
+         height:"50px",
+         backgroundColor:"#66f3",
+      };
+      return <div style={s}></div>
+   }
+
+   render(){
       return <div>
          <h1>React</h1>
-         <p style={this.msgStyle}>{this.state.msg}</p>
-         <p style={this.msgStyle}>{this.props.msg}</p>
-         <Rect x="50" y="50" w="150" h="150" c="#6ff9" r="50" />
-         <Rect x="150" y="100" w="150" h="150" c="#f6f9" r="75" />
-         <Rect x="100" y="150" w="150" h="150" c="#6669" r="25"  />
-      </div>;
+         <h2 style={this.msgStyle}>show rect.</h2>
+         <div style={this.area} onClick={this.doAction}>
+            {this.data.map((value)=>this.draw(value))}
+         </div>
+      </div>
    }
+
 }
 
 export default App;
